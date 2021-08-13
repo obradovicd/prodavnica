95b0a6120fd498a1b4b422d14ca3978fe2363deb
Darinka Obradovic <obradovicd216@gmail.com>
2021-08-13 16:42:25 +0200

    commit

---
+++ resources/views/proizvod.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Butik</title>
+    <link rel="icon"  href="public\images\icon.png"> 
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('./css/app.css') }}">
+
+</head>
+
+<body style="background-color:#D58936">
+    <div id="navbar"></div>
+    <div class="container d-flex justify-items-center">
+        <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src={{ asset('./css/img/' . $proizvod->kategorija . '.jpg') }} class="card-img-top"
+                        alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $proizvod->naziv }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{ $proizvod->kategorija }}</h6>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col">
+                <h3><b>Naziv:</b> {{ $proizvod->naziv }}</h3>
+                <h3> <b> Opis:</b> Ovo <br> je <br> opis  <br> proizvoda</h3>
+                <h3><b> Kategorija:</b> {{ $proizvod->kategorija }}</h3>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="../js/app.js"></script>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Butik</title>
+    <link rel="icon"  href="public\images\icon.png"> 
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('./css/app.css') }}">
+   
+
+</head>
+
+<body>
+    <div id="home"></div>
+    <script src="../js/app.js"></script>
+</body>
+
+</html>
